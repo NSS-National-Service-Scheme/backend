00067ec99c8b440cb99e6c02d5ff515c7fed7141
Kirethik <mohanasundaramdeepa@gmail.com>
2025-07-23 00:04:56 +0530

    Update expense.js

--- utilites/dataValidator/expense.js
+++ utilites/dataValidator/expense.js
@@ -1,5 +1,7 @@
-export const validateExpense = (EventID, Amount, Description ,ImageURL) => {
+const validateExpense = (EventID, Amount, Description ,ImageURL) => {
     if (!EventID || !Amount || !Description ) {
             return 'EventID, Amount, and Description are required fields' ;
     }
 }
+
+export default validateExpense;
